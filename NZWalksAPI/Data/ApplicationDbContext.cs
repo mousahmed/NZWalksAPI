2d6f3623d75686fcb31af654473d8e1cc6231d0f
Moustafa Ahmed <Mous.Ahmed@outlook.com>
2024-09-16 08:04:07 +0100

    Created Models, DBContext & Migrations

---
+++ NZWalksAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+    }
+}
--- NZWalksAPI/Program.cs
+++ NZWalksAPI/Program.cs
@@ -1,8 +1,26 @@
+using dotenv.net;
+using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+DotEnv.Load();
+
+// Build the connection string using environment variables
+var connectionString = string.Format(
+    "Server={0},{1};Database={2};User Id={3};Password={4};TrustServerCertificate=True;",
+    Environment.GetEnvironmentVariable("SQLSERVER_HOST"),
+    Environment.GetEnvironmentVariable("SQLSERVER_PORT"),
+    Environment.GetEnvironmentVariable("SQLSERVER_DB"),
+    Environment.GetEnvironmentVariable("SQLSERVER_USER"),
+    Environment.GetEnvironmentVariable("SQLSERVER_PASSWORD")
+);
+
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
