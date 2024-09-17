8bf99b500d44e3400b0cc790602d9edf3a9fdce5
Moustafa Ahmed <Mous.Ahmed@outlook.com>
2024-09-17 07:59:31 +0100

    Seed data to region & difficulty tables

--- NZWalksAPI/Data/ApplicationDbContext.cs
+++ NZWalksAPI/Data/ApplicationDbContext.cs
@@ -12,5 +12,59 @@ namespace NZWalksAPI.Data
         public DbSet<Walk> Walks { get; set; }
         public DbSet<Region> Regions { get; set; }
         public DbSet<Difficulty> Difficulties { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Region>().HasData(
+                new Region
+                {
+                    Id = new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6d"),
+                    Name = "Auckland",
+                    Code = "AKL",
+                    RegionImageUrl = "https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/auckland/auckland-region-landscape.jpg"
+                },
+                new Region
+                {
+                    Id = new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6e"),
+                    Name = "Wellington",
+                    Code = "WLG",
+                    RegionImageUrl = "https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/wellington/wellington-region-landscape.jpg"
+                },
+                new Region
+                {
+                    Id = new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6f"),
+                    Name = "Canterbury",
+                    Code = "CAN",
+                    RegionImageUrl = "https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/canterbury/canterbury-region-landscape.jpg"
+                },
+                new Region
+                {
+                    Id = new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6c"),
+                    Name = "Otago",
+                    Code = "OTA",
+                    RegionImageUrl = "https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/otago/otago-region-landscape.jpg"
+                }
+            );
+
+            modelBuilder.Entity<Difficulty>().HasData(
+                new Difficulty
+                {
+                    Id = new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6a"),
+                    Name = "Easy",
+
+                },
+                new Difficulty
+                {
+                    Id = new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6b"),
+                    Name = "Moderate",
+                },
+                new Difficulty
+                {
+                    Id = new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6c"),
+                    Name = "Hard",
+                }
+            );
+
+        }
     }
 }
---
+++ NZWalksAPI/Migrations/20240917062730_addDataToTables.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NZWalksAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDataToTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Difficulties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6a"), "Easy" },
+                    { new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6b"), "Moderate" },
+                    { new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6c"), "Hard" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Code", "Name", "RegionImageUrl" },
+                values: new object[,]
+                {
+                    { new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6c"), "OTA", "Otago", "https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/otago/otago-region-landscape.jpg" },
+                    { new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6d"), "AKL", "Auckland", "https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/auckland/auckland-region-landscape.jpg" },
+                    { new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6e"), "WLG", "Wellington", "https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/wellington/wellington-region-landscape.jpg" },
+                    { new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6f"), "CAN", "Canterbury", "https://www.doc.govt.nz/globalassets/images/conservation/parks-and-recreation/places-to-visit/canterbury/canterbury-region-landscape.jpg" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6a"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6b"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6c"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6c"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6d"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6e"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("b3b3e6e1-1b36-4b0e-8e4b-6b4f3f1b3f6f"));
+        }
+    }
+}
