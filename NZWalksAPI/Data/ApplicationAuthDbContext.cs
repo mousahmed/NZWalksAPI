1691f15403b26aca5a9ef650009232b4d62fb507
Moustafa Ahmed <Mous.Ahmed@outlook.com>
2024-09-24 07:57:09 +0100

    Add initial implementation of Identity and authentication

---
+++ NZWalksAPI/Data/ApplicationAuthDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalksAPI.Data
+{
+    public class ApplicationAuthDbContext : IdentityDbContext
+    {
+        public ApplicationAuthDbContext(DbContextOptions<ApplicationAuthDbContext> options) : base(options)
+        {
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            var readerRoleId = Guid.NewGuid().ToString();
+            var writerRoleId = Guid.NewGuid().ToString();
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "READER"
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "WRITER"
+                }
+            );
+
+        }
+    }
+}
--- NZWalksAPI/Program.cs
+++ NZWalksAPI/Program.cs
@@ -1,9 +1,13 @@
 using AutoMapper;
 using dotenv.net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NZWalksAPI.Data;
 using NZWalksAPI.Mappings;
 using NZWalksAPI.Repositories;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,24 +16,61 @@ var builder = WebApplication.CreateBuilder(args);
 DotEnv.Load();
 
 // Build the connection string using environment variables
-var connectionString = string.Format(
+string APPLICATION_DB = Environment.GetEnvironmentVariable("SQLSERVER_DB");
+string APPLICATION_AUTH_DB = Environment.GetEnvironmentVariable("SQLSERVER_AUTH_DB");
+
+var connectionString = (string SQLSERVER_DB) =>
+{
+    return string.Format(
     "Server={0},{1};Database={2};User Id={3};Password={4};TrustServerCertificate=True;",
     Environment.GetEnvironmentVariable("SQLSERVER_HOST"),
     Environment.GetEnvironmentVariable("SQLSERVER_PORT"),
-    Environment.GetEnvironmentVariable("SQLSERVER_DB"),
+    SQLSERVER_DB,
     Environment.GetEnvironmentVariable("SQLSERVER_USER"),
     Environment.GetEnvironmentVariable("SQLSERVER_PASSWORD")
-);
+    );
+};
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString(APPLICATION_DB)));
+
+builder.Services.AddDbContext<ApplicationAuthDbContext>(options =>
+    options.UseSqlServer(connectionString(APPLICATION_AUTH_DB)));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IRegionRepository, SqlRegionRepository>();
 builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("NZWalks")
+    .AddEntityFrameworkStores<ApplicationAuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 6;
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER"),
+            ValidAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET")))
+        };
+    });
 
 var app = builder.Build();
 
@@ -41,7 +82,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
