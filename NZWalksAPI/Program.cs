58dac05c192ad70015b7b7e917b63e411e2fe297
Moustafa Ahmed <Mous.Ahmed@outlook.com>
2024-09-29 09:18:09 +0100

    Fix deploy workflow

--- NZWalksAPI/Program.cs
+++ NZWalksAPI/Program.cs
@@ -14,7 +14,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// Load environment variables from .env file
 DotEnv.Load();
 
 // Build the connection string using environment variables
